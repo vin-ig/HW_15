d1271e0c8a5fc5f20a7074e55c53297783f16159
Harry <vin-ig@yandex.ru>
2022-04-29 16:13:42 +0300

    table main ready to join

--- animals.sql
+++ animals.sql
@@ -1,35 +1,56 @@
-select * from animals
-limit 10;
+-- Типы животных
 
 create table animal_types(
     id integer primary key autoincrement,
     type varchar(100) not null
 );
 
--- create table age_upon_outcome(
---     id integer primary key autoincrement,
---     age varchar(10) not null
--- );
+insert into animal_types (type)
+select distinct animal_type from animals;
+
+-- Породы
 
 create table breed(
     id integer primary key,
     breed varchar(100) not null
 );
 
+insert into breed (breed)
+select distinct breed from animals;
+
+-- Цвета
+
 create table colors(
     id integer primary key autoincrement,
     color varchar(50) not null
 );
 
+insert into colors (color)
+select distinct color1 from animals;
+
+-- Программы
+
 create table outcome_subtypes(
     id integer primary key autoincrement,
-    subtype varchar(50)
+    subtype varchar(50) not null
 );
+
+insert into outcome_subtypes (subtype)
+select distinct outcome_subtype from animals
+where outcome_subtype not null;
+
+-- Программы
 
 create table outcome_types(
     id integer primary key autoincrement,
-    type varchar(50)
+    type varchar(50) not null
 );
+
+insert into outcome_types (type)
+select distinct outcome_type from animals
+where outcome_type not null;
+
+-- Главная таблица
 
 create table main(
     id  integer primary key autoincrement,
@@ -45,7 +66,6 @@ create table main(
     outcome_type varchar(50),
     outcome_month integer,
     outcome_year integer,
---     foreign key (age_upon_outcome) references age_upon_outcome(age),
     foreign key (animal_type) references animal_types(id),
     foreign key (breed) references breed(id),
     foreign key (color1) references colors(id),
@@ -54,27 +74,36 @@ create table main(
     foreign key (outcome_type) references outcome_types(id)
 );
 
--- Заполняем малые таблицы
-
-insert into animal_type (type)
-select distinct animal_type from animals;
-
-insert into breed (breed)
-select distinct breed from animals;
-
-insert into colors (color)
-select distinct color1 from animals;
-
-insert into outcome_subtypes (subtype)
-select distinct outcome_subtype from animals;
-
-insert into outcome_types (type)
-select distinct outcome_type from animals;
-
 -- Заполняем основную таблицу
 
-update animals
-set breed = id from breed
-where animals.breed = breed.breed
+insert into main (animal_id, animal_type, name, date_of_birth, age_upon_outcome, breed,
+                  color1, color2, outcome_subtype, outcome_type, outcome_month, outcome_year)
+select animal_id, animal_type, name, date_of_birth, age_upon_outcome, breed,
+                  color1, color2, outcome_subtype, outcome_type, outcome_month, outcome_year from animals;
 
+-- Обновляем столбцы
+
+update main
+set breed = (select id from breed
+where main.breed = breed.breed);
+
+update main
+set animal_type = (select id from animal_types
+where main.animal_type = animal_types.type);
+
+update main
+set outcome_subtype = (select id from outcome_subtypes
+where main.outcome_subtype = outcome_subtypes.subtype);
+
+update main
+set outcome_type = (select id from outcome_types
+where main.outcome_type = outcome_types.type);
+
+update main
+set color1 = (select id from colors
+where main.color1 = colors.color);
+
+update main
+set color2 = (select id from colors
+where main.color2 = colors.color);
 
--- utils.py
+++ utils.py
@@ -1,104 +1,99 @@
 import sqlite3
 
-with sqlite3.connect('D:/Temp/HW_15/animal.db') as connection:
+
+def delete_spaces(column):
+
+	query = f'SELECT "index", {column} FROM animals'
+	cursor.execute(query)
+	result = cursor.fetchall()
+	clean_dict = {}
+	for row in result:
+		try:
+			if row[1][-1] == ' ':
+				clean_dict[row[0]] = row[1].strip()
+		except TypeError:
+			continue
+
+	return clean_dict
+
+
+def write_column(data):
+	for key, value in data.items():
+		query = f"""
+			update animals
+			set color1 = '{value}'
+			where "index" = {key}
+			"""
+		cursor.execute(query)
+
+
+with sqlite3.connect('animal.db') as connection:
 	cursor = connection.cursor()
+	clean_dict = delete_spaces('color1')
+	write_column(clean_dict)
+
+
+	query = '''
+			create table breeds(
+			id integer primary key,
+			breed varchar(100) not null
+			)'''
+	# cursor.execute(query)
+
+	query = '''
+			insert into breeds (breed)
+			select distinct breed from animals
+			'''
+	# cursor.execute(query)
+
+	query = '''
+			update animals
+			set breed = (select id from breeds
+			where animals.breed = breeds.breed)
+			'''
+	# cursor.execute(query)
+
+	query = """
+			create table colors(
+				id integer primary key autoincrement,
+				color varchar(50));
+			"""
+	# cursor.execute(query)
+	
+
+
+# query = f'''
+# 		update animals
+# 		set color1 = '{delete_spaces('color1')}'
+# 		'''
+# # cursor.execute(query)
+#
+# ########################################
+# query = """
+# 		insert into colors (color)
+# 		select distinct color1 from animals
+# 		"""
+# # cursor.execute(query)
+#
+# query = '''
+# 		update animals
+# 		set breed = (select id from breeds
+# 		where animals.breed = breeds.breed)
+# 		'''
+# # cursor.execute(query)
+#
+#
+#
+#
+# query = """
+# 		SELECT distinct color1, color2 FROM animals
+# 		"""
+# cursor.execute(query)
+#
+# # result = [i[0] for i in cursor.execute(query).description]
+# result = cursor.fetchall()
+# for row in result:
+# 	print(*row, sep=' - ')
 
-query = """
-		SELECT * FROM animals
-		LIMIT 10
-		"""
-cursor.execute(query)
-# result = [i[0] for i in cursor.execute(query).description]
-result = cursor.fetchall()
 
-for row in result:
-	print(row)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-input()
